6eb146ca3c8bd43dcee59a6b4c4229a621b4b6aa
Cezary Olborski <cezary.olborski@gmail.com>
2024-09-09 16:24:50 +0800

    feat: Domain objects extracted

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
+mod domain;
+
+use std::collections::BTreeMap;
+use std::sync::Arc;
 use binance_spot_connector_rust::{
     market_stream::partial_depth::PartialDepthStream,
     tokio_tungstenite::BinanceWebSocketClient,
 };
 use env_logger::Builder;
 use futures_util::StreamExt;
-
+use ordered_float::OrderedFloat;
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-struct DepthData {
-    #[serde(rename = "lastUpdateId")]
-    last_update_id: u64,
-    bids: Vec<[String; 2]>,
-    asks: Vec<[String; 2]>,
-}
-
-#[derive(Debug, Deserialize)]
-struct StreamData {
-    //#[serde(rename = "stream")]
-    stream: String,
-    data: DepthData,
-}
+use tokio::sync::Mutex;
+use domain::stream_data::StreamData;
+use crate::domain::order_book::OrderBook;
 
 #[tokio::main]
 async fn main() {
+
+    let order_book = Arc::new(Mutex::new(OrderBook::new()));
+
     Builder::from_default_env()
         .filter(None, log::LevelFilter::Info)
         .init();
@@ -33,7 +28,7 @@ async fn main() {
         .expect("Failed to connect");
     // Subscribe to streams
     conn.subscribe(vec![
-        &PartialDepthStream::from_100ms("BTCFDUSD",10).into()
+        &PartialDepthStream::from_100ms("BTCFDUSD",5).into()
     ])
         .await;
     // Read messages
@@ -43,19 +38,16 @@ async fn main() {
                 let binary_data = message.into_data();
                 let data = std::str::from_utf8(&binary_data).expect("Failed to parse message");
                 //let wrapped_data = format!("r#{}#",data);
-                println!("DATA: {}",data);
                 if !data.contains(":null")
                 {
                     let result: StreamData = serde_json::from_str(format!(r#"{}"#,data).trim()).unwrap();
-                    log::info!("#{:?}",result);
+                    let mut book = order_book.lock().await;
+                    book.update(result);
+                    book.print_top_of_book();
                 }
                 else {
                     log::info!("Empty row: {}",data);
                 }
-                //log::info!("{:?}", data);
-                //let json_str = "r#"+data+"#";
-                //println!("JSON_STR: {}",json_str);
-                //let result: StreamData = serde_json::from_slice(&binary_data).expect("Failed to parse message");
             }
             Err(_) => break,
         }
